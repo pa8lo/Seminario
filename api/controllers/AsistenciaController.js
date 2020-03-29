b74e978ba24bbf388fbea13ddf1d771332d08eb8
Rozek <pablo.rozek@externos-ar.cencosud.com>
2020-03-29 19:32:45 -0300

    arreglado asistencia controller

--- api/controllers/AsistenciaController.js
+++ api/controllers/AsistenciaController.js
@@ -162,7 +162,7 @@ module.exports = {
         let fechasalida =data.Asistencia.OutTime.split("/")
         let horasalida = fechasalida[2].split(" ");
         let horaParseadasalida = horasalida[1].split(":")
-        let fechacompletasalida = new Date(horasalida[0],fechasalida[1]-1,fechasalidaha[0],horaParseadasalida[0].trim()-3,horaParseadasalida[1],horaParseadasalida[2],horaParseadasalida[3])
+        let fechacompletasalida = new Date(horasalida[0],fechasalida[1]-1,fechasalida[0],horaParseadasalida[0].trim()-3,horaParseadasalida[1],horaParseadasalida[2],horaParseadasalida[3])
         sails.log.info(fechacompleta);
         let datos = {
           OutTime:fechacompletasalida,
